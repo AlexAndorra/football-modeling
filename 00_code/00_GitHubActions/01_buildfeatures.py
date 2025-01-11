39be78e9b23a1dd7d3237298ae0a24532248f489
Maximilian Göbel <110088382+maxi-tb22@users.noreply.github.com>
2025-01-11 16:59:10 +0100

    Update 01_buildfeatures.py

--- 00_code/00_GitHubActions/01_buildfeatures.py
+++ 00_code/00_GitHubActions/01_buildfeatures.py
@@ -107,7 +107,7 @@ print(f'Number of Players: {len(pd.unique(players_raw["name_player"]))}')
 print(f'Number of Goals:   {int(goals_total)}')
 print(f'Share of Identified Goals: {np.round(scorer_raw.shape[0] / goals_total,3)}')
 
-print('erling-haaland' in players_raw['name_player'])
+print(players_raw.loc[5,'name_player'])
 print('casemiro' in players_raw['name_player'])
 print(players_raw.head(10))
 sys.exit(-1)
